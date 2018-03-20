2699aae40a4e283555dfc84552e697fbe4b6cf46
IZQUIERDO AYALA <pabizay@EVIRL-024-OK.dsic.cloud>
2018-03-21 00:43:15 +0100

    Dictionary as subset change. Now we have lists as subsets and they are in order.

--- mOnO InfInItO/SAR_p3_monkey_indexer.py
+++ mOnO InfInItO/SAR_p3_monkey_indexer.py
@@ -10,15 +10,33 @@ def main(filename, resultname):
         textdollar = re.sub(r'\n', " ", re.sub(r'\n\n', " $ ", re.sub(r'[;.?!]', " $ ", texto)))
         finaltext = re.sub("[^\w$]"," ", textdollar).split()
         hashtable = {}
+#        for i in range(len(finaltext)-1):
+#            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, {finaltext[i+1]:0}])
+#            hashtable[finaltext[i]][0] += 1
+#            aux = hashtable[finaltext[i]][1]
+#            aux[finaltext[i+1]] = aux.get(finaltext[i+1], 0) + 1
+#            hashtable[finaltext[i]][1] = aux
+            
+            
         for i in range(len(finaltext)-1):
-            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, {finaltext[i+1]:0}])
+            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, [(0,finaltext[i+1])]])
             hashtable[finaltext[i]][0] += 1
             aux = hashtable[finaltext[i]][1]
-            aux[finaltext[i+1]] = aux.get(finaltext[i+1], 0) + 1
-            hashtable[finaltext[i]][1] = aux
+            te = True
+            listatemp = []
+            for elemento in aux:
+                if(elemento[1] == finaltext[i+1]):
+                    listatemp.append(tuple((elemento[0]+1, elemento[1])))
+                    te = False
+                else:
+                    listatemp.append(elemento)
+            if(te):
+                listatemp.append(tuple((1,finaltext[i+1])))
+            hashtable[finaltext[i]][1] = sorted(listatemp, key=lambda x: x[0], reverse = True)
             
-        print(hashtable)
-        print(hashtable["$"])
+        
+        for key in sorted(hashtable.keys()):
+            print(hashtable[key])
                 
     
     
