0e9c6b59d174220b8f00ed90526ac9af6c461f3d
IZQUIERDO AYALA <pabizay@EVIRL-024-OK.dsic.cloud>
2018-03-20 22:04:05 +0100

    Tokenization

--- mOnO InfInItO/SAR_p3_monkey_indexer.py
+++ mOnO InfInItO/SAR_p3_monkey_indexer.py
@@ -1,8 +1,15 @@
 import re
+import sys
 
 def main(filename, resultname):
-   with open(filename , 'r') as origin:
-    phrase = re.compile((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\:|\!)\s)
+    with open(filename , 'r') as origin:
+    #phrase = re.compile((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\:|\!)\s))
+        #jumpreg = re.compile
+        texto = "$ " + origin.read() + " $"
+        textdollar = re.sub(r'\n', " ", re.sub(r'\n\n', " $ ", re.sub(r'[;.?!]', " $ ", texto)))
+        finaltext = re.sub("[^\w$]"," ", textdollar).split()        
+        print(finaltext)
+    
     
 
 def syntax():
