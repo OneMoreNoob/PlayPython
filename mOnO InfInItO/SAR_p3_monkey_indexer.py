c07c4c3475a58a7f04a5abda1e6a53873b986c08
IZQUIERDO AYALA <pabizay@EVIRL-024-OK.dsic.cloud>
2018-03-20 23:35:21 +0100

    Hashtable fully operative

--- mOnO InfInItO/SAR_p3_monkey_indexer.py
+++ mOnO InfInItO/SAR_p3_monkey_indexer.py
@@ -5,10 +5,21 @@ def main(filename, resultname):
     with open(filename , 'r') as origin:
     #phrase = re.compile((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\:|\!)\s))
         #jumpreg = re.compile
-        texto = "$ " + origin.read() + " $"
+        textrg = "$ " + origin.read() + " $"
+        texto = textrg.lower()
         textdollar = re.sub(r'\n', " ", re.sub(r'\n\n', " $ ", re.sub(r'[;.?!]', " $ ", texto)))
-        finaltext = re.sub("[^\w$]"," ", textdollar).split()        
-        print(finaltext)
+        finaltext = re.sub("[^\w$]"," ", textdollar).split()
+        hashtable = {}
+        for i in range(len(finaltext)-1):
+            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, {finaltext[i+1]:0}])
+            hashtable[finaltext[i]][0] += 1
+            aux = hashtable[finaltext[i]][1]
+            aux[finaltext[i+1]] = aux.get(finaltext[i+1], 0) + 1
+            hashtable[finaltext[i]][1] = aux
+            
+        print(hashtable)
+        print(hashtable["$"])
+                
     
     
 
