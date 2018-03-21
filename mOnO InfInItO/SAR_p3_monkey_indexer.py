b5c4b8b15343d7a937681d75a5e729802a275c48
IZQUIERDO AYALA <pabizay@EVIRL-024-OK.dsic.cloud>
2018-03-21 01:01:04 +0100

    Pickle added

--- mOnO InfInItO/SAR_p3_monkey_indexer.py
+++ mOnO InfInItO/SAR_p3_monkey_indexer.py
@@ -1,15 +1,15 @@
 import re
 import sys
+import pickle
 
 def main(filename, resultname):
     with open(filename , 'r') as origin:
-    #phrase = re.compile((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\:|\!)\s))
-        #jumpreg = re.compile
         textrg = "$ " + origin.read() + " $"
         texto = textrg.lower()
         textdollar = re.sub(r'\n', " ", re.sub(r'\n\n', " $ ", re.sub(r'[;.?!]', " $ ", texto)))
         finaltext = re.sub("[^\w$]"," ", textdollar).split()
         hashtable = {}
+#      This subset implementation was made using dictionaries        
 #        for i in range(len(finaltext)-1):
 #            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, {finaltext[i+1]:0}])
 #            hashtable[finaltext[i]][0] += 1
@@ -32,14 +32,15 @@ def main(filename, resultname):
                     listatemp.append(elemento)
             if(te):
                 listatemp.append(tuple((1,finaltext[i+1])))
-            hashtable[finaltext[i]][1] = sorted(listatemp, key=lambda x: x[0], reverse = True)
-            
+            hashtable[finaltext[i]][1] = sorted(listatemp, key=lambda x: x[0], reverse = True)            
         
-        for key in sorted(hashtable.keys()):
-            print(hashtable[key])
+        save_object(hashtable, resultname)
                 
     
     
+def save_object(object, file_name):
+    with open(file_name, 'wb') as fh:
+        pickle.dump(object, fh)
 
 def syntax():
     print ("\n%s filename.txt resultfile.txt\n" % sys.argv[0])
