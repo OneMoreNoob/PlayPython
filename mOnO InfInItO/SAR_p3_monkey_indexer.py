6b3ea574a95fd44e378efc1115f71fa0828622d3
IZQUIERDO AYALA <pabizay@EVIRL-026-OK.dsic.cloud>
2018-03-21 14:01:49 +0100

    Efficiency modifications, code improved (Akshay hates past tense)

--- mOnO InfInItO/SAR_p3_monkey_indexer.py
+++ mOnO InfInItO/SAR_p3_monkey_indexer.py
@@ -9,35 +9,43 @@ def main(filename, resultname):
         textdollar = re.sub(r'\n', " ", re.sub(r'\n\n', " $ ", re.sub(r'[;.?!]', " $ ", texto)))
         finaltext = re.sub("[^\w$]"," ", textdollar).split()
         hashtable = {}
-#      This subset implementation was made using dictionaries        
-#        for i in range(len(finaltext)-1):
-#            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, {finaltext[i+1]:0}])
-#            hashtable[finaltext[i]][0] += 1
-#            aux = hashtable[finaltext[i]][1]
-#            aux[finaltext[i+1]] = aux.get(finaltext[i+1], 0) + 1
-#            hashtable[finaltext[i]][1] = aux
-            
-            
+        
+#      This subset implementation was made using dictionaries   
+
         for i in range(len(finaltext)-1):
-            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, [(0,finaltext[i+1])]])
+            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, {finaltext[i+1]:0}])
             hashtable[finaltext[i]][0] += 1
             aux = hashtable[finaltext[i]][1]
-            te = True
-            listatemp = []
-            for elemento in aux:
-                if(elemento[1] == finaltext[i+1]):
-                    listatemp.append(tuple((elemento[0]+1, elemento[1])))
-                    te = False
-                else:
-                    listatemp.append(elemento)
-            if(te):
-                listatemp.append(tuple((1,finaltext[i+1])))
-            hashtable[finaltext[i]][1] = listatemp
+            aux[finaltext[i+1]] = aux.get(finaltext[i+1], 0) + 1
+            hashtable[finaltext[i]][1] = aux
+        
+        for key in hashtable.keys():
+            dic = hashtable[key][1] 
+            hashtable[key][1] = sorted([(dic[x], x) for x in  dic.keys()], reverse = True)
+            
+#       print(hashtable)
+#     This version is less efficient and doesn't use dic
+
+#        for i in range(len(finaltext)-1):
+#            hashtable[finaltext[i]] = hashtable.get(finaltext[i], [0, [(0,finaltext[i+1])]])
+#            hashtable[finaltext[i]][0] += 1
+#            aux = hashtable[finaltext[i]][1]
+#            te = True
+#            listatemp = []
+#            for elemento in aux:
+#                if(elemento[1] == finaltext[i+1]):
+#                    listatemp.append(tuple((elemento[0]+1, elemento[1])))
+#                    te = False
+#                else:
+#                    listatemp.append(elemento)
+#          if(te):
+#               listatemp.append(tuple((1,finaltext[i+1])))
+#            hashtable[finaltext[i]][1] = listatemp
             
             #hashtable[finaltext[i]][1] = sorted(listatemp, reverse = True)            
         
-        for key in hashtable.keys():
-            hashtable[key][1] = sorted(hashtable[key][1], reverse = True)
+#        for key in hashtable.keys():
+#            hashtable[key][1] = sorted(hashtable[key][1], reverse = True)
         save_object(hashtable, resultname)
                 
     
