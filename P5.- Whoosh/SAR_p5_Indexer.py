6f8a892c55cc90e5a00adf6ee2e35d7bbf29635f
PUNJABI <akpun@EVIRL-006-OK.dsic.cloud>
2018-04-27 14:30:10 +0200

    Indexer implementation

--- P5.- Whoosh/SAR_p5_Indexer.py
+++ P5.- Whoosh/SAR_p5_Indexer.py
@@ -6,17 +6,18 @@ from whoosh.fields import Schema, ID, TEXT
 
 schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
 idir = "index_dir"
-
+pathenero = "./enero"
 if not os.path.exists(idir):
     os.mkdir(idir)
 ix = create_in(idir, schema)
 
+
+directory = os.fsencode(pathenero)
 writer = ix.writer()
-writer.add_document(title="First document", path="/a",
-                    content="Este es el texto del primer documento_")
-writer.add_document(title="Second document", path="/b",
-                    content="Este es el TEXTO de nuestro segundo documento")
+print(os.listdir(directory))
+for f in os.listdir(directory):
+    print(os.fsdecode(f))
+    with open(pathenero + "/" + os.fsdecode(f) , 'r' ) as origin:
+        writer.add_document(title=os.fsdecode(f), path=pathenero,
+                    content=origin.read())        
 writer.commit()
-
-
-
