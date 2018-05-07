303df074808921d908f33168722a7bacab82971f
Dauriel <pabizay@EVIRL-024-OK.dsic.cloud>
2018-05-07 20:03:58 +0200

    Indexer 2.0

--- P5.- Whoosh/SAR_p5_Indexer_2.py
+++ P5.- Whoosh/SAR_p5_Indexer_2.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 
 import os
+import re
 from whoosh.index import create_in
 from whoosh.fields import Schema, ID, TEXT
+
 #from whoosh.analysis import LowercaseFilter, RegexTokenizer
-schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+schema = Schema(title=TEXT(stored=True), path=ID(stored=True), globalID=TEXT(stored=True), inDoc=TEXT(stored=True), content=TEXT)
 idir = "index_dir"
 pathenero = "./enero"
 if not os.path.exists(idir):
     os.mkdir(idir)
 ix = create_in(idir, schema)
 
-
 directory = os.fsencode(pathenero)
 writer = ix.writer()
 for f in os.listdir(directory):
     with open(pathenero + "/" + os.fsdecode(f) , 'r' ) as origin:
-        writer.add_document(title=os.fsdecode(f), path=pathenero,
-                    content=origin.read())        
-writer.commit()
+        counter = 0
+        texto = origin.read()
+        noticias = re.compile('<text>(.*?)</text>', re.DOTALL |  re.IGNORECASE).findall(texto)
+        titulos = re.compile('<title>(.*?)</title>', re.DOTALL |  re.IGNORECASE).findall(texto)
+        notit = zip(titulos, noticias)
+        for par in notit:
+            counter += 1
+            writer.add_document(title = par[0],path=pathenero, globalID= os.fsdecode(f)+ "/" + str(counter),inDoc= str(counter),content= par[1])
+writer.commit()        
