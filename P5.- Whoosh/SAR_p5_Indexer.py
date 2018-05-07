953d9de4ef748d5de21d583a1a4e80d47d94ef9a
Dauriel <pabizay@EVIRL-024-OK.dsic.cloud>
2018-05-07 18:18:26 +0200

    Ex 1.3

--- P5.- Whoosh/SAR_p5_Indexer.py
+++ P5.- Whoosh/SAR_p5_Indexer.py
@@ -3,7 +3,7 @@
 import os
 from whoosh.index import create_in
 from whoosh.fields import Schema, ID, TEXT
-
+#from whoosh.analysis import LowercaseFilter, RegexTokenizer
 schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT)
 idir = "index_dir"
 pathenero = "./enero"
@@ -14,9 +14,7 @@ ix = create_in(idir, schema)
 
 directory = os.fsencode(pathenero)
 writer = ix.writer()
-print(os.listdir(directory))
 for f in os.listdir(directory):
-    print(os.fsdecode(f))
     with open(pathenero + "/" + os.fsdecode(f) , 'r' ) as origin:
         writer.add_document(title=os.fsdecode(f), path=pathenero,
                     content=origin.read())        
--- P5.- Whoosh/SAR_p5_Searcher.py
+++ P5.- Whoosh/SAR_p5_Searcher.py
@@ -2,17 +2,19 @@
 
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
+from whoosh.query import *
 
 ix = open_dir("index_dir")
 with ix.searcher() as searcher:
-    while True:
-        text = input("Dime:")
-        if len(text) == 0:
-            break
-        query = QueryParser("content", ix.schema).parse(text)
-        results = searcher.search(query)
-#        print(dir(results))
-#        print(results.docs)
-        for r in results:
-            print(r)
-
+    valencia = QueryParser("content", ix.schema).parse("valencia")
+    resvalencia = searcher.search(valencia)
+    print(len(resvalencia))
+    notSalenko = QueryParser("content", ix.schema).parse("valencia NOT Salenko")
+    resnotSalenko = searcher.search(notSalenko)
+    print(len(resnotSalenko))
+    futbol = QueryParser("content", ix.schema).parse("futbol")
+    resFutbol = searcher.search(futbol)
+    print(len(resFutbol))
+    aero = QueryParser("content", ix.schema).parse("Los Angeles AND Aeroflot")
+    resAero = searcher.search(aero)
+    print(len(resAero))
