3e61b8ded56684cd714b66caf1b12943d03d7fb9
Dauriel <pabizay@EVIRL-024-OK.dsic.cloud>
2018-05-07 20:41:47 +0200

    Ex2

--- P5.- Whoosh/SAR_p5_Searcher_2.py
+++ P5.- Whoosh/SAR_p5_Searcher_2.py
@@ -4,17 +4,38 @@ from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
 from whoosh.query import *
 
+def printer(lista):
+    print("Número de elementos encontrados en la consulta: ", len(lista), "\n")
+    if(len(lista) <4):
+        cuatro(lista)
+    else:
+        for item in lista:
+            formattedPrint(item)
+                
+def formattedPrint(diccionario):
+    for keys  in sorted(diccionario.keys()):
+                print(keys, ": " , diccionario[keys].strip())
+    print("")
+
+def cuatro(lista):
+    for item in lista: 
+        with open(item["path"] + "/" + item["globalID"].split("/")[0] , 'r' ) as origin:
+            noticias = re.compile('<text>(.*?)</text>', re.DOTALL |  re.IGNORECASE).findall(origin.read())
+            formattedPrint(item)
+            print("Contenido:\n", noticias[int(item["inDoc"])-1])                
+       
+        
 ix = open_dir("index_dir")
 with ix.searcher() as searcher:
     valencia = QueryParser("content", ix.schema).parse("valencia")
     resvalencia = searcher.search(valencia)
-    print(len(resvalencia))
+    printer(resvalencia)
     notSalenko = QueryParser("content", ix.schema).parse("valencia NOT Salenko")
     resnotSalenko = searcher.search(notSalenko)
-    print(len(resnotSalenko))
+    printer(resnotSalenko)
     futbol = QueryParser("content", ix.schema).parse("futbol")
     resFutbol = searcher.search(futbol)
-    print(len(resFutbol))
+    printer(resFutbol)
     aero = QueryParser("content", ix.schema).parse("Los Angeles AND Aeroflot")
     resAero = searcher.search(aero)
-    print(len(resAero))
+    printer(resAero)
